350cd556026a0a26c8e5db93da364c8071e4d57c
Ashish Khatri <44234ashish@gmail.com>
2025-04-14 16:54:56 +0530

    Add files via upload

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 import axios from 'axios';
 
-const API_URL = "https://api-car.onrender.com" || "http://localhost:5000/api"; 
+const API_URL = 'http://localhost:5000/api'; // Change if your backend is on a different port
 
 export const registerUser = async (userData) => {
   return axios.post(`${API_URL}/auth/register`, userData);
