e67b8590d36d762396d4ae7a30b4bdd793e7d0b9
AshishKhatri84 <44234ashish@gmail.com>
2025-04-19 23:09:46 +0530

    Updates
    Updated

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api'; // Change if your backend is on a different port
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api'; // Change if your backend is on a different port
 
 export const registerUser = async (userData) => {
   return axios.post(`${API_URL}/auth/register`, userData);
